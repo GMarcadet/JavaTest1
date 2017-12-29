28611825b5eb0e9d1f068b6b4abe8a9bf649c507
Gael Marcadet <gael@mbp-de-gael.home>
2017-12-29 12:51:22 +0100

    test 2

--- javaTest.java
+++ javaTest.java
@@ -4,5 +4,7 @@ public class TestGitHub{
 	public static void main(String[] args){
 		System.out.println("Java Test Release 1.0");
 	}
-
+	public static void test(){
+		System.out.println("hello"+var);
+	}
 }
